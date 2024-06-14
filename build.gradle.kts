4162447e02fc5a3dae236fd328565069d23f6717
cvetyshayasiren <quadthirteen4@gmail.com>
2024-06-15 00:48:02 +0300

    hello CellularAutomatonKMP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.internal.sharedruntime.codegen.pluginEntriesFrom
-
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -8,12 +6,5 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-}
-
-subprojects {
-    if(project.name == "CellularAutomatonCompose") {
-        apply {
-            plugin("maven-publish")
-        }
-    }
+    `maven-publish`
 }
