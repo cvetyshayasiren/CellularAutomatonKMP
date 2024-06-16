0acbece4a8b42022ba5ed0291adc0fac22e329c7
cvetyshayasiren <quadthirteen4@gmail.com>
2024-06-16 03:31:27 +0300

    hello CellularAutomatonKMP

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,22 +8,10 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    `maven-publish`
     java
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
-}
-
-subprojects {
-    if(this.name == "CellularAutomatonCompose") {
-        apply { plugin("maven-publish")}
-        publishing {
-            publications {
-                create<MavenPublication>("maven")
-            }
-        }
-    }
 }
